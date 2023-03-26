64c08cb46248d4b25d8f65428e9d295e28171ddd
Chase Harler <cHWc8889@gmail.com>
2023-03-26 00:01:38 -0500

    Adding practice about crates & modules

---
+++ restaurant/target/debug/deps/restaurant-060f65362e01a29f.d
@@ -0,0 +1,9 @@
+C:\Users\chwc8\GitHub\rust_projects\restaurant\target\debug\deps\restaurant-060f65362e01a29f.rmeta: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
+
+C:\Users\chwc8\GitHub\rust_projects\restaurant\target\debug\deps\librestaurant-060f65362e01a29f.rlib: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
+
+C:\Users\chwc8\GitHub\rust_projects\restaurant\target\debug\deps\restaurant-060f65362e01a29f.d: src\lib.rs src\front_of_house.rs src\front_of_house\hosting.rs
+
+src\lib.rs:
+src\front_of_house.rs:
+src\front_of_house\hosting.rs:
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+C:\Users\chwc8\GitHub\rust_projects\restaurant\target\debug\librestaurant.rlib: C:\Users\chwc8\GitHub\rust_projects\restaurant\src\front_of_house\hosting.rs C:\Users\chwc8\GitHub\rust_projects\restaurant\src\front_of_house.rs C:\Users\chwc8\GitHub\rust_projects\restaurant\src\lib.rs
